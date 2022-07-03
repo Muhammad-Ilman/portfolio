b1e97e270f4a4178b11f193ffd55b4057b0d391d
Muhammad-Ilman <pratamailman06@gmail.com>
2022-07-03 23:07:24 +0700

    [1.56.55] swiper sectionn

--- js/script.js
+++ js/script.js
@@ -1,17 +1,36 @@
 const header = document.querySelector("header");
+
 const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
+
 const progress_bars = document.querySelectorAll(".skills svg circle");
+const ml_section = document.querySelector(".milestones");
+const ml_counters = document.querySelectorAll(".number span");
+
+const prt_section = document.querySelector(".portfolio");
+const zoom_icons = document.querySelectorAll(".zoom-icon");
+const modal_overlay = document.querySelector(".modal-overlay");
+const images = document.querySelectorAll(".images img");
+const prev_btn = document.querySelector(".prev-btn");
+const next_btn = document.querySelector(".next-btn");
+
+
+let mlPlayed = false;
+
+window.addEventListener("scroll", () => {
+  if (!skilsPlayed) skillsCounter();
+  if (!mlPlayed) mlCounter();
+});
 
 // SKILLS
-const hasReached = (el) => {
+function hasReached(el) {
   let topPosition = el.getBoundingClientRect().top;
-  if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+  if (window.innerHeight >= topPosition + el.offsetHeight)
+    return true;
   return false;
-
 }
 
-const updateCounter = (num, maxNum) => {
+function updateCounter(num, maxNum) {
   let curentNum = +num.innerText;
 
   if (curentNum < maxNum) {
@@ -25,10 +44,10 @@ const updateCounter = (num, maxNum) => {
 
 let skilsPlayed = false;
 
-const skillsCounter = () => {
-  if (!hasReached(first_skill)) return;
+function skillsCounter() {
+  if (!hasReached(first_skill))
+    return;
   // console.log(`Hello you scroll the skill`);
-
   let skilsPlayed = true;
 
   sk_counters.forEach((counter, i) => {
@@ -42,17 +61,13 @@ const skillsCounter = () => {
     }, 400);
 
     // console.log(typeof  target, strokeValue, progress_bars);
-  })
+  });
 
   progress_bars.forEach(p => p.style.animation = "progress 2s ease-in-out forwards");
 }
 
-window.addEventListener("scroll", () => {
-  if(!skilsPlayed) skillsCounter();
-})
-
 // sticky navbar
-const stickyNavbar = () => {
+function stickyNavbar() {
   // console.log(window.scrollY > 0)
   header.classList.toggle("scrolled", window.scrollY > 0);
 }
@@ -63,6 +78,74 @@ window.addEventListener("scroll", stickyNavbar);
 if (!header) {
   alert("tag header not found")
 }
+
+// services
+
+function mlCounter() {
+  if (!hasReached(ml_section)) return;
+
+  let mlPlayed = true;
+
+  ml_counters.forEach(ctr => {
+    let target = +ctr.dataset.target;
+
+    console.log(ctr)
+
+    setTimeout(() => {
+      updateCounter(ctr, target)
+    }, 400);
+  });
+}
+
+// modal
+let currentIndex = 0;
+
+zoom_icons.forEach((icn, i) =>
+  icn.addEventListener("click", () => {
+    prt_section.classList.add("open");
+    document.body.classList.add("stopScrolling");
+    currentIndex = i;
+    changeImage(currentIndex);
+  }));
+
+modal_overlay.addEventListener("click", () => {
+  prt_section.classList.remove("open");
+  document.body.classList.remove("stopScrolling");
+});
+
+prev_btn.addEventListener("click", () => {
+  if (currentIndex === 0) {
+    currentIndex = 5;
+  } else {
+    currentIndex--;
+  }
+  changeImage(currentIndex);
+})
+
+next_btn.addEventListener("click", () => {
+  if (currentIndex === 5) {
+    currentIndex = 0;
+  } else {
+    currentIndex++;
+  }
+  changeImage(currentIndex);
+})
+
+function changeImage(index) {
+  images.forEach(img => img.classList.remove("showImage"));
+  images[index].classList.add("showImage");
+}
+
+// filter animation
+let mixer = mixitup(".portfolio-gallery", {
+  selectors: {
+    target: ".prt-card",
+  },
+  animation: {
+    duration: 500,
+  }
+});
+
 
 let sr = ScrollReveal({
   duration: 2500,
@@ -75,4 +158,15 @@ sr.reveal(".showcase-info", {
 sr.reveal(".showcase-image", {
   origin: "top",
   delay: 700
+});
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  speed: 500,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
 });
